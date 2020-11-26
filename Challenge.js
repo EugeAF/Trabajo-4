35bdee631c92d8ca3f237a27356dd46e97f57c27
Euge <eugeniaariasfagalde22@gmail.com>
2020-11-26 12:16:15 -0300

    Challenge Warm Up

---
+++ Challenge.js
@@ -0,0 +1,10 @@
+var numeroTres = prompt("Ingrese el primer numero por favor:");
+var numeroCuatro = prompt("Ingrese el segundo numero por favor:");
+
+if(numeroTres == numeroCuatro) {
+    alert("Son iguales.");
+}else if(numeroTres > numeroCuatro) {
+    alert("El primer numero es mayor que el segundo.")
+}else if(numeroCuatro > numeroTres){
+    alert("El segundo numero es mayor que el primero")
+}
