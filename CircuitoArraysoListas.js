fb43532f67811918fb846206a2ec2f3924951a0f
Euge <eugeniaariasfagalde22@gmail.com>
2020-11-26 12:16:45 -0300

    Circuito Arrays

---
+++ CircuitoArraysoListas.js
@@ -0,0 +1,17 @@
+var listaEquipo = [];
+
+listaEquipo.push("Bart")
+
+console.log(listaEquipo)
+
+listaEquipo.unshift("Lisa")
+listaEquipo.push("Milhouse")
+listaEquipo.push("Tino")
+
+console.log(listaEquipo)
+console.log(listaEquipo[3])
+
+listaEquipo.shift(0)
+listaEquipo.pop(3)
+
+console.log(listaEquipo)
