ce8fa5c07959241fc1a85a4cd6d5fc4d44265ad0
Euge <eugeniaariasfagalde22@gmail.com>
2020-11-26 12:17:37 -0300

    Warm Up

---
+++ WarmUp.js
@@ -0,0 +1,10 @@
+var numeroUno = 5;
+var numeroDos = 20;
+
+if(numeroUno == numeroDos) {
+    console.log("Son iguales.");
+}else if(numeroUno > numeroDos) {
+    console.log("El primer numero es mayor que el segundo.")
+}else if(numeroUno < numeroDos){
+    console.log("El segundo numero es mayor que el primero")
+}
